ab27f3a0dee3735a39e7f9c4e7263e0d64b50c61
Nicole Tolch <email": null>
2015-11-20 10:00:03 -0600

    include full_name virtual attributes in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,15 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  def full_name
+    [first_name, last_name].join(' ')
+  end
+
+  def full_name=(name)
+    split = name.split(' ', 2)
+    self.first_name = split.first
+    self.last_name = split.last
+  end
+
+
 end

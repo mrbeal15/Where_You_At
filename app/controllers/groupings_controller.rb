016a08278e5625417cafceabade7ce9f72972989
McEileen & Lorena <pair+McEileen.dclorena11@devbootcamp.com>
2015-11-22 14:43:06 -0600

    edit users controller

--- app/controllers/groupings_controller.rb
+++ app/controllers/groupings_controller.rb
@@ -3,14 +3,7 @@ class GroupingsController < ApplicationController
   def create
     @user = User.find_by(id: current_user)
     @group = Group.find_by(id: params[:id])
-    #see if current_user is made
-    # @grouping = @user.groupings.create(group_id: @group.id, user_id: @user.id, joined?: true)
-    #redirect to group show page
-    @grouping = Grouping.where(group_id: @group.id, user_id: @user_id)
-
-    if @grouping
-      @grouping.update_attributes(joined?: true)
-    end
+    @grouping = Grouping.where(group_id: @group.id, user_id: @user_id, joined?: true)
   end
 
   def destroy

c281d59e6aa2ccb74a4273d86e26621c64d73481
Lorena <email": null>
2015-11-23 11:00:28 -0600

    fix create grouping controller

--- app/controllers/groupings_controller.rb
+++ app/controllers/groupings_controller.rb
@@ -1,17 +1,23 @@
 class GroupingsController < ApplicationController
 
   def create
-    @user = User.find_by(id: current_user)
-    @group = Group.find_by(id: params[:id])
-    @grouping = Grouping.where(group_id: @group.id, user_id: @user_id, joined?: true)
+    @user = User.find_by(email: grouping_params[:email])
+    @group = Group.find_by(id: grouping_params[:group_id])
+    @grouping = Grouping.create(group_id: @group.id, user_id: @user.id, joined?: true)
+    if @grouping
+      render :json => @grouping
+    end
   end
 
   def destroy
     @grouping = Grouping.find_by(id: params[:id])
     @grouping.destroy
-    #redirect to user's show page
   end
 
+  private
 
+  def grouping_params
+    params.permit(:email, :group_id)
+  end
 
 end

d51138b8aa5346a862d8a0274cc476e3b238c263
McEileen & Nicole Tolch <pair+McEileen.ntolch@devbootcamp.com>
2015-11-24 11:42:29 -0600

    Update groupings#create @group params

--- app/controllers/groupings_controller.rb
+++ app/controllers/groupings_controller.rb
@@ -2,7 +2,7 @@ class GroupingsController < ApplicationController
 
   def create
     @user = User.find_by(email: grouping_params[:email])
-    @group = Group.find_by(id: grouping_params[:group_id])
+    @group = Group.find(params[:id])
     @grouping = Grouping.create(group_id: @group.id, user_id: @user.id, joined?: true)
     if @grouping
       render :json => @grouping

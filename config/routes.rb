04d105c2e6ae1447d8fa4d911da4993caf11c723
Lorena <email": null>
2015-11-24 13:40:20 -0600

    add coordinates route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
 
+  post '/users/:id/coordinates' => 'users#coordinates'
   post '/groups/:id/invite' => 'groupings#create'
   get '/groups/:id/invite' => 'group#invite'
   get '/groups/:id/map' => 'groups#show_map'#, as: "map"

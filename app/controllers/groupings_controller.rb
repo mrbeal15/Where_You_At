28a6a5e9bc09d6cf7919f269d8828ccc1faeeb4b
McEileen & Nicole Tolch <pair+McEileen.ntolch@devbootcamp.com>
2015-11-24 11:45:59 -0600

    some logging

--- app/controllers/groupings_controller.rb
+++ app/controllers/groupings_controller.rb
@@ -3,7 +3,10 @@ class GroupingsController < ApplicationController
   def create
     @user = User.find_by(email: grouping_params[:email])
     @group = Group.find(params[:id])
+    Rails.logger.info(@group.inspect)
+    Rails.logger.info(@user.inspect)
     @grouping = Grouping.create(group_id: @group.id, user_id: @user.id, joined?: true)
+    Rails.logger.info(@grouping.inspect)
     if @grouping
       render :json => @grouping
     end

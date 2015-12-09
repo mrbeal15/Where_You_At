1782b6534879df64df85c2b147be2bf5631e5778
Eileen McFarland <eileenm@gmail.com>
2015-12-09 11:54:59 -0600

    delete current user and logged_in? helper methods that we didn't use

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,2 @@
 module ApplicationHelper
-
-  def current_user
-    @current_user ||= User.find_by_id(session[:user])
-  end
-
-  def logged_in?
-    current_user != nil
-  end
-
-  # helper_method :current_user
-  # helper_method :logged_in?
-
 end
